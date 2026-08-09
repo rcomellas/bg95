fa6baae5f0456153d4508db7bfa05d4d1e3cd166
Ramon Comellas <rcomellas@hotmail.com>
2026-08-09 14:31:21 +0200

    prova ota. nomes canvio versio

--- src/ota/main.py
+++ src/ota/main.py
@@ -9,6 +9,7 @@ import atcmd
 from misc import Power
 from umqtt import MQTTClient
 import app_fota
+from machine import WDT
 
 from usr import xtra
 from usr import config
@@ -16,22 +17,26 @@ from usr import secrets
 # endregion imports
 
 # region Constants
-VERSIO = "2026-08-05-1"
+VERSIO = "1.0.1"
+DEBUG = True
 TAU_DEMANAT = 1800  # per tal que la xarxa em doni 60 minuts, en demano 30
+# segons. Per defecte 6 segons, que és el mínim que permet la xarxa. Si es vol més temps, cal demanar-ho a l'operador.
+ACTIVE_TIME = 6
 TOPIC_ORDRES = b"bg95/command"
 BASE_URL_OTA = "https://raw.githubusercontent.com/rcomellas/bg95/main/src/ota/"
 _thread.stack_size(16 * 1024)
 # endregion
 
+wdt = WDT(180)  # en segons
 fitxers_ota_pendents = None
 
 
 def obtenir_posicio():
+    debug("TEMPS_MAXIM_FIX:", config.TEMPS_MAXIM_FIX)
     inici = time.ticks_ms()
 
     while time.ticks_diff(time.ticks_ms(), inici) < config.TEMPS_MAXIM_FIX * 1000:
         resultat = quecgnss.read(4096)
-
         if resultat != -1 and resultat[0] > 0:
             dades = resultat[1]
 
@@ -60,6 +65,7 @@ def obtenir_posicio():
                 satel_lits = int(gga[7]) if gga and gga[7] else 0
 
                 return latitud, longitud, satel_lits
+        debug("Sense fix GNSS. Esperant 2 segons...")
         time.sleep(2)
 
     return None
@@ -76,6 +82,11 @@ def convertir_coordenada(valor, hemisferi, graus):
 
 def temps_transcorregut(inici):
     return time.ticks_diff(time.ticks_ms(), inici) // 1000
+
+
+def debug(*args):
+    if DEBUG:
+        print(*args)
 
 
 def obtenir_psm_negociat():
@@ -128,7 +139,6 @@ def publicar_mqtt(posicio, status):
             "id": config.DEVICE_ID,
             "latitude": latitud,
             "longitude": longitud,
-            "bat": Power.getVbatt(),
             "sat": satel_lits
         }
 
@@ -149,7 +159,8 @@ def publicar_mqtt(posicio, status):
     if fitxers_ota_pendents:
         executar_ota(fitxers_ota_pendents, client)
         return
-
+    debug(posicio, status)
+    time.sleep(1)
     client.disconnect()
 
 
@@ -204,9 +215,12 @@ def main():
     inici = time.ticks_ms()
     stage, state = checkNet.waitNetworkReady(30)
     net_time = temps_transcorregut(inici)
+    wdt.feed()
 
     if stage == 3 and state == 1:
         try:
+            quecgnss.gnssEnable(0)
+
             xtra.actualitzar_si_cal()
         except Exception as error:
             pass
@@ -218,12 +232,11 @@ def main():
             gnss_time = 0
 
         else:
-            quecgnss.gnssEnable(1)
-
             inici = time.ticks_ms()
             posicio = obtenir_posicio()
             gnss_time = temps_transcorregut(inici)
-
+            wdt.feed()
+            debug("Posició obtinguda en", gnss_time, "segons:", posicio)
             quecgnss.gnssEnable(0)
 
         unitat_tau, valor_tau = (
@@ -232,24 +245,27 @@ def main():
             else (1, TAU_DEMANAT // 3600)
         )
 
-        pm.set_psm_time(unitat_tau, valor_tau, 0, 15)
+        pm.set_psm_time(unitat_tau, valor_tau, 0, ACTIVE_TIME // 2)
 
         time.sleep(5)
 
         tau_net, active_time = obtenir_psm_negociat()
 
         status = {
+            "version": VERSIO,
+            "bat": Power.getVbatt(),
             "tau_req": TAU_DEMANAT,
             "tau_net": tau_net,
             "active_time": active_time,
             "net_time": net_time,
             "gnss_time": gnss_time,
-            "fix": posicio is not None,
-            "versio": VERSIO
+            "fix": posicio is not None
         }
 
         try:
             publicar_mqtt(posicio, status)
+            debug("Publicat MQTT posicio: ", posicio, "status:", status)
+            wdt.feed()
         except Exception as error:
             pass
 
