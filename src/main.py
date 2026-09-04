9ae2b5a74f33ad6181ca1b19dbd5c3d8e14dc68e
Ramon Comellas <rcomellas@hotmail.com>
2026-09-04 17:25:13 +0200

    OTA v1.0.33

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # main.py — Tracker BG95-M3
-VERSIO = "1.0.32"
+VERSIO = "1.0.33"
 
 import utime, ujson, quecgnss, pm, checkNet, _thread, atcmd, app_fota
 import ntptime, uos, net, dataCall, ubinascii, uhashlib
