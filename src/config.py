c77e192eb563f4d9a5c3fa46ee276e37951890e2
Ramon Comellas <rcomellas@hotmail.com>
2026-09-20 20:20:08 +0200

    OTA v1.0.54

--- src/config.py
+++ src/config.py
@@ -22,7 +22,7 @@ TEMPS_MAXIM_FIX = 100  # segons
 
 # PSM
 TAU_CURT = 180  # en segons
-TAU_LLARG = 180  # en segons
+TAU_LLARG = 5000  # en segons
 ACTIVE_TIME = 10  # en segons
 HORA_INICI_TAU_LLARG = 0
 HORA_FINAL_TAU_LLARG = 7
